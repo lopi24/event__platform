b20bb1613acb04c9a507a73213a5a8a51ded1a52
lopi24 <louelp24@gmail.com>
2024-03-25 18:34:39 +0800

    implement stripe

--- app/(root)/events/create/page.tsx
+++ app/(root)/events/create/page.tsx
@@ -1,22 +1,27 @@
-import EventForm from "@/components/shared/EventForm"
+import EventForm from "@/components/shared/EventForm";
 import { auth } from "@clerk/nextjs";
 
 const CreateEvent = () => {
+  // - auth hook from clerk
   const { sessionClaims } = auth();
-
+  // -- extracting userId from the hook
+  // -- this will pass to the session page in clerk (see the edit)
   const userId = sessionClaims?.userId as string;
+  console.log(userId);
 
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
-        <h3 className="wrapper h3-bold text-center sm:text-left">Create Event</h3>
+        <h3 className="wrapper h3-bold text-center sm:text-left">
+          Create Event
+        </h3>
       </section>
 
       <div className="wrapper my-8">
         <EventForm userId={userId} type="Create" />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default CreateEvent
+export default CreateEvent;
